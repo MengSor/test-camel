5344a639f1797963097a6ef8f8fc6f08114fa131
SovannMengsor <mengsorse@gmail.com>
2024-09-12 16:41:33 +0700

    update

--- src/main/java/com/example/testcamel/MyRoute.java
+++ src/main/java/com/example/testcamel/MyRoute.java
@@ -1,17 +1,35 @@
 package com.example.testcamel;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRoute extends RouteBuilder {
+    @Autowired
+    private OpenAPI openAPI;
+
     @Override
     public void configure() throws Exception {
+//        OpenAPI openAPI = new OpenAPI();
+//        Info info = new Info()
+//                .title("My Api")
+//                .version("1.0")
+//                .description("This is my API");
+//        openAPI.info(info);
+
         restConfiguration()
                 .component("servlet")
                 .contextPath("/api")
-                .port(8080)
+                .apiContextPath("/api-docs")
+                .apiContextRouteId("api-docs")
+                .apiProperty("api.title", openAPI.getInfo().getTitle())
+                .apiProperty("api.version", openAPI.getInfo().getVersion())
+                .apiProperty("base.path", "/api")
+                .apiProperty("cors", "true")
+                .apiProperty("openapi", openAPI.toString())
                 .bindingMode(RestBindingMode.json);
 
         rest("/users")
@@ -24,19 +42,22 @@ public class MyRoute extends RouteBuilder {
                 .post("")
                 .routeId("Post")
                 .to("direct:save");
-
-        from("direct:getUser")
-                .bean(UserRepository.class , "getUser");
-
-        from("direct:save")
-                .log("Received a save request : ${body}")
-                .marshal().json()
-                .unmarshal().json(User.class)
-                .bean(UserRepository.class, "save(${body})");
-
-        from("direct:getUserById")
-                .log("Received header: ${header.id}")
-                .bean(UserRepository.class ,"getUserById(${header.id})");
+//
+//        from("direct:getUser")
+//                .bean(UserRepository.class , "getUser");
+//
+//        from("direct:save")
+//                .log("Received a save request : ${body}")
+//                .marshal().json()
+//                .unmarshal().json(User.class)
+//                .bean(UserRepository.class, "save(${body})");
+//
+//        from("direct:getUserById")
+//                .log("Received header: ${header.id}")
+//                .bean(UserRepository.class ,"getUserById(${header.id})");
+//
+//        from("file:message/foo")
+//                .to("jms:queue:foo");
 
     }
 }
--- src/main/java/com/example/testcamel/OpenApiBean.java
+++ src/main/java/com/example/testcamel/OpenApiBean.java
@@ -1,2 +1,19 @@
-package com.example.testcamel;public class OpenApiBean {
+package com.example.testcamel;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiBean {
+    @Bean
+    public OpenAPI openAPI () {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My Api")
+                        .version("1.0")
+                        .description("This is my API")
+                );
+    }
 }
